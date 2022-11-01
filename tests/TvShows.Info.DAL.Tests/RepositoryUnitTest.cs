bfdf0a9308a454a7b13b9b9df7cdf8a9da4829c3
Shaun Peusschers <peusscherss@gmail.com>
2022-11-01 21:48:03 +0100

    Added summy test to get code coverage to run.

--- tests/TvShows.Info.DAL.Tests/RepositoryUnitTest.cs
+++ tests/TvShows.Info.DAL.Tests/RepositoryUnitTest.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Abstractions;
+using TvShows.Info.DAL.Context.Models;
+
 namespace TvShows.Info.DAL.Tests
 {
     [TestClass]
@@ -7,11 +11,30 @@ namespace TvShows.Info.DAL.Tests
         public void RepositoryAddCastMember()
         {
             // Arrange
-            
+            var options = new DbContextOptionsBuilder<TvShowDbContext>()
+            .UseInMemoryDatabase(databaseName: "MovieListDatabase")
+            .Options;
+
+            var castMember = new CastMember
+            {
+                Name = "Test Member",
+                Bitrthday = DateTime.Now.AddYears(-25)
+            };
+
             // Act
+            using (var context = new TvShowDbContext(options, new NullLoggerFactory()))
+            {
+                context.Database.EnsureCreated();
+                context.CastMembers.Add(castMember);
+                context.SaveChanges();
+            }
 
             //Assert
-
+            using (var context = new TvShowDbContext(options, new NullLoggerFactory()))
+            {
+                var castMemberResult = context.CastMembers.FirstOrDefault();
+                Assert.AreEqual(castMemberResult?.Id, castMember.Id);
+            }
         }
     }
 }

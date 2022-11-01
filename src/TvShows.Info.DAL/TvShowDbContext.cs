6c37775927242075dc9b56e142030b699af6067f
Shaun Peusschers <peusscherss@gmail.com>
2022-11-01 21:21:34 +0100

    Added more projects and test projects.

---
+++ src/TvShows.Info.DAL/TvShowDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using TvShows.Info.DAL.Context.Models;
+
+namespace TvShows.Info.DAL
+{
+    public class TvShowDbContext : DbContext
+    {
+        public readonly ILoggerFactory _loggerFactory;
+
+        public TvShowDbContext(DbContextOptions<TvShowDbContext> options, ILoggerFactory loggerFactory)
+            : base(options)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
+        public DbSet<TvShow> TvShows => Set<TvShow>();
+
+        public DbSet<CastMember> CastMembers => Set<CastMember>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseLoggerFactory(_loggerFactory);
+    }
+}
---
+++ tests/TvShows.Info.DAL.Tests/RepositoryUnitTest.cs
@@ -0,0 +1,17 @@
+namespace TvShows.Info.DAL.Tests
+{
+    [TestClass]
+    public class RepositoryUnitTest
+    {
+        [TestMethod]
+        public void RepositoryAddCastMember()
+        {
+            // Arrange
+            
+            // Act
+
+            //Assert
+
+        }
+    }
+}
